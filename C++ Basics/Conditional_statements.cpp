1344cf71b833977e1b99e28ac9db98f9d1f82e82
Pratham15-18 <prathamdogra454@gmail.com>
2026-05-27 23:04:49 +0530

    Add conditional statements example

---
+++ C++ Basics/Conditional_statements.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age :"<<endl;
+    cin>>age;
+
+    if(age>=18){
+        cout<<"You are eligible for vote"<<endl;
+    }else{
+        cout<<"You are a child ! Go home"<<endl;
+    }
+    return 0;
+}
+    
