1b84c5616fab9b305f022be566af5975214235e9
Pratham15-18 <prathamdogra454@gmail.com>
2026-05-29 17:17:02 +0530

    sum of n || while loop

---
+++ C++ Basics/while_n_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    int n = 3;
+    int sum = 0;
+    
+
+    while(i<=n){
+        sum+=i;
+        i++;
+      
+    }
+
+    
+    cout<<"sum = "<< sum << endl;
+
+  return 0;
+}
