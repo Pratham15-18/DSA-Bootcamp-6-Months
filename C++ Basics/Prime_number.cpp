70dd0af99466c43ce52f1d9b24b1860e7243dcdf
Pratham15-18 <prathamdogra454@gmail.com>
2026-05-30 02:32:27 +0530

    Root n factor in prime no

--- C++ Basics/Prime_number.cpp
+++ C++ Basics/Prime_number.cpp
@@ -7,7 +7,7 @@ int main(){
     cin>>n;
     bool isPrime = true;
 
-    for(int i=2; i<=n-1; i++){
+    for(int i=2; i*i <= n; i++){
         if(n%i == 0){
            isPrime = false;
            break;
