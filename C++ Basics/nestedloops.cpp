d6bc1732ce3078f9626ea2b35c259d4cbd0960ef
Pratham15-18 <prathamdogra454@gmail.com>
2026-06-02 17:28:18 +0530

    Nested loop program

--- C++ Basics/nestedloops.cpp
+++ C++ Basics/nestedloops.cpp
@@ -1,2 +1,14 @@
-//will be adding soon
-//nested loop is one of the core concepts 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+    for(int i=1; i<=n; i++){
+        int m=6;
+        for(int j=1; j<=m; j++){
+            cout<<"*"; 
+        }
+        cout<<endl;
+    }
+}
