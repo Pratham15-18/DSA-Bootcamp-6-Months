eed29f03618b0cbe904b93bea281f166d10b44cc
Pratham15-18 <prathamdogra454@gmail.com>
2026-06-07 22:06:46 +0530

    Hollow diamond pattern

--- Patterns/Hollow_diamond_pattern.cpp
+++ Patterns/Hollow_diamond_pattern.cpp
@@ -1,1 +1,43 @@
 // Hollow diamond pattern in C++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+
+    for(int i=0; i<n; i++){
+        //spaces
+       for(int j=0; j<n-i-1; j++){
+           cout << " ";
+         }
+        cout << "*";
+
+
+        if(i !=0){
+            //spaces
+           for(int j=0; j<2*i-1; j++){
+           cout << " ";
+         }
+
+
+            cout << "*";
+
+
+    }
+    cout << endl;
+  }
+//bottom half
+  for(int i=0; i<n-1; i++){
+    //spaces
+    for(int j=0; j<i+1; j++){
+      cout << " ";
+    }
+
+      cout << "*";
+
+    cout << endl;
+  }
+  return 0;
+}
+
